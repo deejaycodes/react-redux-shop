ff0b2cf529a937f5879b29b098fb6bfae73c8a79
Ayodeji_PC <dejiodetayo@gmail.com>
2021-01-18 13:12:55 +0100

    index file updated

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/build/public"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "public", "index.html")
+    );
   });
 }
 
